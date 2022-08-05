d373fffe16c0381d7123d5595f161921ab209aef
mathias <mathias.hansson@selfpoint.se>
2022-08-05 10:44:14 +0200

    update

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const storybookBoilerplate = require('./src/storybookBoilerplate')
 const sp3WebpackPlugin = require('./src/sp3-webpack-plugin')
 const discoverExports = require('./src/discoverExports')
-const createComponent = require('./src/createComponent')
 
 module.exports = {
     storybookBoilerplate : storybookBoilerplate.default,
     sp3WebpackPlugin: sp3WebpackPlugin.default,
-    discoverExports: discoverExports.default,
-    createComponent: createComponent.default
+    discoverExports: discoverExports.default
 }
